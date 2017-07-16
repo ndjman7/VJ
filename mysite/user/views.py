d722aa9c70d7fc6cb484b9ec1ee551470d0f880d
ndjman7 <ndjman7@gmail.com>
2017-07-16 16:44:37 +0900

    사용자 회원가입, 로그인, 로그아웃 구현
    views, urls, forms를 한 번에 구현하였음.

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import VJLoginForm, VJSignUpForm
 
-# Create your views here.
+def vj_login(request):
+    if request.method == 'POST':
+        form = VJLoginForm(request.POST)
+        if form.is_valid():
+            userid = form.cleaned_data['userid']
+            password = form.cleaned_data['password']
+            user = authenticate(request, userid =userid, password = password)
+        else:
+            return render(request, 'user/login.html', {'form': form})
+        # user의 값이 is_active도 체크
+        if user is not None and user.is_active:
+            login(request, user)
+            return render(request, 'main/main.html', {})
+    else:
+        return render(request, 'user/login.html', {'form': VJLoginForm()})
+
+def vj_signup(request):
+    if request.method == 'POST':
+        form = VJSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user:login')
+        else:
+            return render(request, 'user/signup.html', {'form': form})
+    else:
+        return render(request, 'user/signup.html', {'form': VJSignUpForm()})
+
+def vj_logout(request):
+    logout(request)
+    return redirect('user:login')
